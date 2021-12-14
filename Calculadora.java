dde35bc4f0c378633d19a5a337d7cd5138892542
mandaapag03 <paganiamanda791@gmail.com>
2021-12-14 10:43:13 -0300

    Projetos

---
+++ Calculadora.java
@@ -0,0 +1,27 @@
+package addHabilidades;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner teclado = new Scanner (System.in);
+        System.out.println("Informe o 1° número: ");
+        double n1 = teclado.nextDouble();
+        System.out.println("Informe o operador: ");
+        String op = teclado.next();
+        op = op.replace('x','*');
+        System.out.println("Informe o 2° número: ");
+        double n2 = teclado.nextDouble();
+
+        double resultado = "+".equals(op) ? n1 + n2 : 0;
+        resultado = "-".equals(op) ? n1 - n2 : resultado;
+        resultado = "*".equals(op) ? n1 * n2 : resultado;
+        resultado = "/".equals(op) ? n1 / n2 : resultado;
+        resultado = "%".equals(op) ? n1 % n2 : resultado;
+
+        System.out.printf("%.2f %s %.2f = %.2f", n1, op, n2, resultado);
+        teclado.close();
+    }
+}
---
+++ notasDosAlunos.java
@@ -0,0 +1,38 @@
+package addHabilidades;
+
+import javax.swing.*;
+import java.util.Locale;
+
+public class notasDosAlunos {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        System.out.println("Cálculo de média anual");
+        String nome = JOptionPane.showInputDialog("Digite seu nome: ");
+        String bim1 = JOptionPane.showInputDialog("Informe sua nota de 1°bimestre: ");
+        System.out.print("1°bimestre: " + bim1);
+        bim1 = bim1.replace(',','.');
+        String bim2 = JOptionPane.showInputDialog("Informe sua nota de 2°bimestre: ");
+        System.out.print("\n2°bimestre: " + bim2);
+        bim2 = bim2.replace(',','.');
+        String bim3 = JOptionPane.showInputDialog("Informe sua nota de 3°bimestre: ");
+        System.out.print("\n3°bimestre: " + bim3);
+        bim3 = bim3.replace(',','.');
+        String bim4 = JOptionPane.showInputDialog("Informe sua nota de 4°bimestre: ");
+        System.out.print("\n4°bimestre: " + bim4);
+        bim4 = bim4.replace(',','.');
+        Double b1 = Double.parseDouble(bim1);
+        Double b2 = Double.parseDouble(bim2);
+        Double b3 = Double.parseDouble(bim3);
+        Double b4 = Double.parseDouble(bim4);
+
+        double media = (b1 + b2 + b3 + b4) / 4;
+        String resultado = media >= 6 ? "Parabéns! você foi aprovade!" : "Sinto muito,você foi reprovade";
+
+        System.out.printf("\n" +
+                "%s,sua média anual é %.1f" +
+                "\n%s",nome, media, resultado);
+
+
+
+    }
+}
