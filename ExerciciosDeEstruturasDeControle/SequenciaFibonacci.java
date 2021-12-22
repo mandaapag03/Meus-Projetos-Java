36058fc0c5163f9dfbc3e79d5d141335f3bd0568
mandaapag03 <paganiamanda791@gmail.com>
2021-12-22 11:03:42 -0300

    commit

---
+++ ExerciciosDeEstruturasDeControle/SequenciaFibonacci.java
@@ -0,0 +1,21 @@
+package addMaisHabilidades.ExerciciosDeEstruturasDeControle;
+
+import java.util.Scanner;
+
+public class SequenciaFibonacci {
+        /*Leia um número positivo do usuário, então,
+calcule e imprima a sequencia Fibonacci até o
+primeiro número superior ao número lido.
+Exemplo: se o usuário informou o número 30,
+a sequencia a ser impressa será 0 1 1 2 3 5 8
+13 21 34 */
+    public static void main(String[] args) { // Fn = F(n-1) + F(n-2)
+        Scanner entrada = new Scanner (System.in);
+        int a = 0;
+        int b = 1;
+
+
+
+        entrada.close();
+    }
+}
