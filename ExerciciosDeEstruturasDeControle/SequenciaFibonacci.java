0aee3bcee8249f41a95a86cd2ebcc4bdd8065304
mandaapag03 <paganiamanda791@gmail.com>
2021-12-23 12:29:57 -0300

    commit

--- ExerciciosDeEstruturasDeControle/SequenciaFibonacci.java
+++ ExerciciosDeEstruturasDeControle/SequenciaFibonacci.java
@@ -11,11 +11,16 @@ a sequencia a ser impressa será 0 1 1 2 3 5 8
 13 21 34 */
     public static void main(String[] args) { // Fn = F(n-1) + F(n-2)
         Scanner entrada = new Scanner (System.in);
-        int a = 0;
-        int b = 1;
+        System.out.println("Digite um número: ");
+        int num = entrada.nextInt();
+        int total = 0;
 
-
-
+        for (int i = 0; i <= num; i++){
+            int Fibonacci = (i - total) + (i - (total-1));
+            int fibo = (Fibonacci - total) + (Fibonacci - (total-1));
+            System.out.println(fibo);
+            total = i;
+        }
         entrada.close();
     }
 }
