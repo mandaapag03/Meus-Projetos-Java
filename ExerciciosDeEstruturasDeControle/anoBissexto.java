a5b2febc8e7f2b43a5f359a374c3a5b4d390481f
mandaapag03 <paganiamanda791@gmail.com>
2022-03-30 11:03:12 -0300

    Atualiza ano bissexto e apaga pasta classes

--- ExerciciosDeEstruturasDeControle/anoBissexto.java
+++ ExerciciosDeEstruturasDeControle/anoBissexto.java
@@ -6,7 +6,8 @@ public class anoBissexto {
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner (System.in);
 		int ano;
-		System.out.println("Informe o ano: ");
+		System.out.println("**Bem vindo à calculadora de anos bissextos**");
+		System.out.println("Informe um ano no passado ou futuro para saber se ele é bissexto: ");
 		ano = entrada.nextInt();
 		
 		if (ano % 400 == 0 || ano % 4 == 0 && !(ano % 100 == 0)) {
